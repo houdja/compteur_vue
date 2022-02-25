5b004e25a3e07b3640c0f1a68dabd0dd228590b9
houdja test <houdja-abdallahpro@hotmail.com>
2022-02-25 21:13:32 +0400

    modif du contenu

--- index.php
+++ index.php
@@ -12,11 +12,7 @@ require_once 'component/header.php';
 <section class="description">
     <div class="container">
         <h1 class="title">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <div class="contain_circle">
-            <div class="circle"></div>
-            <div class="circle"></div>
-            <div class="circle"></div>
-        </div>
+        <div class="pic"></div>
         <p class="descr_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem laboriosam debitis, officia aliquam a optio facere, sunt dolores quod maiores enim deserunt ea, ex vitae sed at reprehenderit ipsam mollitia rem reiciendis est dolorum perspiciatis saepe? Eligendi, modi corrupti quisquam, suscipit quas blanditiis maiores ad quod dignissimos debitis excepturi id ex non deserunt inventore, quidem veniam voluptates saepe porro ipsam harum vero iure. Cumque sequi, vitae expedita quisquam ex asperiores. Quos ipsam iure corrupti est beatae pariatur, id quidem officiis cupiditate ducimus explicabo incidunt quod distinctio exercitationem iusto accusamus nobis ut quo tempora maiores illo. Cumque quia et aspernatur vero!</p>
     </div>
 </section>
