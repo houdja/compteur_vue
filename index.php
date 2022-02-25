4895b522910ceb7579938019f4c0980c6f528b9f
houdja test <houdja-abdallahpro@hotmail.com>
2022-02-25 20:49:46 +0400

    structure de la page d'accueil

--- index.php
+++ index.php
@@ -3,6 +3,24 @@ $title = 'Accueil';
 require_once 'component/header.php';
 ?>
 
+<nav>
+    <div class="container">
+        <h1>Eyes</h1>
+    </div>
+</nav>
+
+<section class="decription">
+    <div class="container">
+        <div class="title">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+        <div class="contain_circle">
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="circle"></div>
+        </div>
+        <div class="descr_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem laboriosam debitis, officia aliquam a optio facere, sunt dolores quod maiores enim deserunt ea, ex vitae sed at reprehenderit ipsam mollitia rem reiciendis est dolorum perspiciatis saepe? Eligendi, modi corrupti quisquam, suscipit quas blanditiis maiores ad quod dignissimos debitis excepturi id ex non deserunt inventore, quidem veniam voluptates saepe porro ipsam harum vero iure. Cumque sequi, vitae expedita quisquam ex asperiores. Quos ipsam iure corrupti est beatae pariatur, id quidem officiis cupiditate ducimus explicabo incidunt quod distinctio exercitationem iusto accusamus nobis ut quo tempora maiores illo. Cumque quia et aspernatur vero!</div>
+    </div>
+</section>
+
 <?php
 require_once 'component/footer.php';
 ?>
