b9c1e7faa0f69e2ce72c0150af7afc562d832fd5
houdja test <houdja-abdallahpro@hotmail.com>
2022-02-26 22:10:02 +0400

    creation de la page dashboard

---
+++ screen/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+if ($_SESSION['role'] != 'admin') {
+    header("http://localhost/compteur_vue/screen/connexion.php");
+}
+require_once '../component/header.php';
+?>
+<section class="section">
+    <div class="container">
+        <p>Bienvenue <?= $_SESSION['login'] ?></p>
+    </div>
+</section>
+<?php
+require_once '../component/footer.php';
+?>
