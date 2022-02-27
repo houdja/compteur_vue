4f326efe81d49ba3f0ff72ba7340bd75af8c8ef3
houdja test <houdja-abdallahpro@hotmail.com>
2022-02-27 16:16:13 +0400

    menu vertical

---
+++ component/vertical_nav.php
@@ -0,0 +1,19 @@
+<div class="vertical_nav">
+    <div class="top">
+        <a href="http://localhost/compteur_vue/screen/dashboard.php" data-descrp='Tableau de bord'>
+            <span class="material-icons">
+                home
+            </span>
+        </a>
+        <a href="http://localhost/compteur_vue/screen/details_vue.php" data-descrp='Détails des vues'>
+            <span class="material-icons">
+                calendar_month
+            </span>
+        </a>
+    </div>
+    <a href="http://localhost/compteur_vue/screen/dashboard.php?<?php echo 'logout' ?>" class='logout' data-descrp='Déconnexion'>
+        <span class="material-icons">
+            logout
+        </span>
+    </a>
+</div>
