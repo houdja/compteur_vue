ce06c95f0aa9458d3e6d3385fea325514fa9f5d0
houdja test <houdja-abdallahpro@hotmail.com>
2022-02-27 13:55:28 +0400

    creation de navigation verttical + all_vue

--- screen/dashboard.php
+++ screen/dashboard.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if (isset($_GET['logout'])) {
+    $_SESSION = array();
+}
 if (isset($_SESSION['role']) && $_SESSION['role'] != "admin" || empty($_SESSION)) {
     header("location:http://localhost/compteur_vue/screen/connexion.php");
 }
@@ -7,7 +10,22 @@ require_once '../component/header.php';
 ?>
 <section class="section dasboard">
     <div class="container">
-        <p>Bienvenue <?= $_SESSION['login'] ?></p>
+        <div class="vertical_nav">
+            <a href="#" data-descrp='Vues'>
+                <span class="material-icons">
+                    visibility
+                </span>
+            </a>
+            <a href="http://localhost/compteur_vue/screen/dashboard.php?<?php echo 'logout' ?>" class='logout' data-descrp='Déconnexion'>
+                <span class="material-icons">
+                    logout
+                </span>
+            </a>
+        </div>
+        <div class="all_vues">
+            <h1>Nombre total de vues</h1>
+            <div class="nbr_vue"></div>
+        </div>
     </div>
 </section>
 <?php
