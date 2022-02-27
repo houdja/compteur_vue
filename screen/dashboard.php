1149e1b718ded955f513a810284bda1539a1e254
houdja test <houdja-abdallahpro@hotmail.com>
2022-02-27 14:25:51 +0400

    ajout des fichier connexiondb + total vue

--- screen/dashboard.php
+++ screen/dashboard.php
@@ -6,6 +6,8 @@ if (isset($_GET['logout'])) {
 if (isset($_SESSION['role']) && $_SESSION['role'] != "admin" || empty($_SESSION)) {
     header("location:http://localhost/compteur_vue/screen/connexion.php");
 }
+require_once '../logic/authentification/connexiondb.php';
+require_once '../logic/vues/total_vue.php';
 require_once '../component/header.php';
 ?>
 <section class="section dasboard">
@@ -23,8 +25,8 @@ require_once '../component/header.php';
             </a>
         </div>
         <div class="all_vues">
-            <h1>Nombre total de vues</h1>
-            <div class="nbr_vue"></div>
+            <h1>Nombre total de vues sur le site</h1>
+            <div class="nbr_vue"><?= htmlentities($total_vue['total_vue']) ?></div>
         </div>
     </div>
 </section>
