990bef492133d1f739166ac04418211c8a5e3444
houdja test <houdja-abdallahpro@hotmail.com>
2022-02-27 16:16:39 +0400

    remplacement du code par un require de verticalnav

--- screen/dashboard.php
+++ screen/dashboard.php
@@ -12,25 +12,7 @@ require_once '../component/header.php';
 ?>
 <section class="section dasboard">
     <div class="container">
-        <div class="vertical_nav">
-            <div class="top">
-                <a href="http://localhost/compteur_vue/screen/dashboard.php" data-descrp='Tableau de bord'>
-                    <span class="material-icons">
-                        home
-                    </span>
-                </a>
-                <a href="#" data-descrp='Détails des vues'>
-                    <span class="material-icons">
-                        calendar_month
-                    </span>
-                </a>
-            </div>
-            <a href="http://localhost/compteur_vue/screen/dashboard.php?<?php echo 'logout' ?>" class='logout' data-descrp='Déconnexion'>
-                <span class="material-icons">
-                    logout
-                </span>
-            </a>
-        </div>
+        <?php require_once '../component/vertical_nav.php'; ?>
         <div class="all_vues">
             <h1>Nombre total de vues sur le site</h1>
             <div class="nbr_vue"><?= htmlentities($total_vue['total_vue']) ?></div>
